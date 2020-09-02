da2657cc8941a16f0e5ba598f1d202698fc8472e
cimpli2005 <70048710+cimpli2005@users.noreply.github.com>
2020-09-02 23:16:17 +0700

    Store Post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Posts\CreatePostsRequest;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -13,7 +15,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index')->with('posts', Post::all());
     }
 
     /**
@@ -32,9 +34,22 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostsRequest $request)
     {
-        //
+        //upload image to storage
+        //dd($request->image->store('posts'));
+        $image = $request->image->store('posts');
+        //create the post
+        Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'image' => $image
+        ]);
+        //flash message
+        session()->flash('success', 'Post created successfully.');
+        //redirect
+        return redirect(route('posts.index'));
     }
 
     /**
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 <div class="card card-default">
     <div class="card-header">Create Post</div>
     <div class="card-body">
-        <form action="posts.store" method="POST">
+        <form action="{{route('posts.store')}}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="form-group"><label for="title">Title</label><input type="text" class="form-control" name="title" id="title"></div>
             <div class="form-group"><label for="description">Description</label><textarea name="description" id="description" cols="5" rows="5" class="form-control"></textarea></div>
